25b48fab699cb61522b14cac22e097750bd539e4
Will Holen <willholen@fb.com>
2021-02-09 12:29:35 -0800

    Lol

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
+all: Test.class test
+
+
 Test.class: Test.java
 	javac Test.java
 
+test: test.cpp
+	c++ test.cpp -o test
---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <iostream>
+
+int main() {
+  std::string foo = "foo";
+  if (foo == "bar") {
+    std::cout << foo << "\n";
+  }
+  return 0;
+}
