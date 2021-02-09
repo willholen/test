0e19e55d781aa66b9c7e5f95b3d6681fb66d4b5a
Will Holen <willholen@fb.com>
2021-02-09 12:49:19 -0800

    Lol

--- test.cpp
+++ test.cpp
@@ -1,10 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include <string>
 #include <iostream>
 
 int main() {
   std::string foo = "foo";
   if (foo == "bar") {
-    std::cout << foo << "\n";
+    fopen(foo.c_str(), "w");
   }
   return 0;
 }
